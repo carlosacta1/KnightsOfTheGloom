fd96df8008a6162b42a2374a51f78c3a7265947b
Carlos Acosta <carlos428.ca@gmail.com>
2023-07-22 20:12:14 -0500

    Adding the game loop

--- app/src/main/java/com/example/knightsofthegloom/Game.java
+++ app/src/main/java/com/example/knightsofthegloom/Game.java
@@ -1,13 +1,19 @@
 package com.example.knightsofthegloom;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 public class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private GameLoop gameLoop;
+    private Context context;
+
     public Game(Context context) {
         super(context);
 
@@ -15,12 +21,15 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         SurfaceHolder surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
 
-        GameLoop gameLoop = new GameLoop(this, surfaceHolder);
+        this.context = context;
+        gameLoop = new GameLoop(this, surfaceHolder);
+
+        setFocusable(true);
     }
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
-
+        gameLoop.startLoop();
     }
 
     @Override
@@ -31,5 +40,33 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
 
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+    }
+
+    public void drawUPS(Canvas canvas) {
+        String averageUPS = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("UPS: " + averageUPS, 100, 100, paint);
+    }
+
+    public void drawFPS(Canvas canvas) {
+        String averageFPS = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("FPS: " + averageFPS, 100, 200, paint);
+    }
+
+    public void update() {
     }
 }
--- app/src/main/java/com/example/knightsofthegloom/GameLoop.java
+++ app/src/main/java/com/example/knightsofthegloom/GameLoop.java
@@ -1,8 +1,116 @@
 package com.example.knightsofthegloom;
 
+import android.graphics.Canvas;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 
-public class GameLoop {
+import java.util.Observer;
+
+public class GameLoop extends Thread {
+
+    private static final double MAX_UPS = 30.0;
+    private static final double UPS_PERIOD = 1E+3/MAX_UPS;
+
+    private Game game;
+    private SurfaceHolder surfaceHolder;
+
+    private boolean isRunning = false;
+    private double averageUPS;
+    private double averageFPS;
+
     public GameLoop(Game game, SurfaceHolder surfaceHolder) {
+        this.game = game;
+        this.surfaceHolder = surfaceHolder;
+    }
+
+    public double getAverageUPS() {
+        return averageUPS;
+    }
+
+    public double getAverageFPS() {
+        return averageFPS;
+    }
+
+    public void startLoop() {
+        isRunning = true;
+        start();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        //Declare count and cycle count variables
+        int updateCount = 0;
+        int frameCount = 0;
+
+        long startTime;
+        long elapsedTime;
+        long sleepTime;
+
+        //Game Loop
+        Canvas canvas = null;
+        startTime = System.currentTimeMillis();
+
+        while(isRunning) {
+            try {
+                canvas = surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    game.update();
+                    updateCount++;
+                    game.draw(canvas);
+                }
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                        frameCount++;
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            //Pause game loop
+            elapsedTime = System.currentTimeMillis() - startTime;
+            sleepTime = (long) (updateCount*UPS_PERIOD - elapsedTime);
+            if(sleepTime > 0) {
+                try {
+                    sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Skip frames to keep up with target ups
+            while(sleepTime < 0 && updateCount < MAX_UPS-1) {
+                game.update();
+                updateCount++;
+                elapsedTime = System.currentTimeMillis() - startTime;
+                sleepTime = (long) (updateCount*UPS_PERIOD - elapsedTime);
+            }
+
+
+            //Calculate average UPS and FPS
+            elapsedTime = System.currentTimeMillis() - startTime;
+            if (elapsedTime >= 1000) {
+                averageUPS = updateCount / (1E-3 * elapsedTime);
+                averageFPS = frameCount / (1E-3 * elapsedTime);
+                updateCount = 0;
+                frameCount = 0;
+                startTime = System.currentTimeMillis();
+            }
+        }
+    }
+    public void stopLoop() {
+        isRunning = false;
+        try {
+            join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
